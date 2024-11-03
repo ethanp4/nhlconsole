4c9d82f3043bfdb6cdd2bd7dfc1089ce06089af2
ethanp4 <157554972+ethanp4@users.noreply.github.com>
2024-11-02 19:47:30 -0600

    start error handling for query

--- nhlconsole/Program.cs
+++ nhlconsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.VisualBasic.FileIO;
 using System.Linq.Dynamic.Core;
+using System.Xml.Linq;
 
 namespace nhlconsole {
     internal class Program {
@@ -8,28 +9,104 @@ namespace nhlconsole {
         static List<csvRow> handleQuery(string query) {
             var list = csvParser.getRows().AsQueryable(); //cast to queryable to use dynamic linq
             var split = query.Split(' '); //ie { "GP", ">=", "50" }
+            if (split.Length > 2) {
+                Console.WriteLine("handle multiple queries");
+            }
 
-            var where = $"{split[0]} {split[1]} {split[2]}";
+            //name = cols[0],
+            //team = cols[1],
+            //pos = cols[2],
+            //gp = int.Parse(cols[3]),
+            //g = int.Parse(cols[4]),
+            //a = int.Parse(cols[5]),
+            //p = int.Parse(cols[6]),
+            //plusMinus = int.Parse(cols[7]),
+            //pim = int.Parse(cols[8]),
+            //pOverGp = float.TryParse(cols[9], out _) ? float.Parse(cols[9]) : -999, // this one can be "--", replace this with -999 then interpret that as --
+            //ppg = int.Parse(cols[10]),
+            //ppp = int.Parse(cols[11]),
+            //shg = int.Parse(cols[12]),
+            //shp = int.Parse(cols[13]),
+            //gwg = int.Parse(cols[14]),
+            //otg = int.Parse(cols[15]),
+            //s = int.Parse(cols[16]),
+            //sPercent = float.Parse(cols[17]),
+            //toiOverGp = cols[18],
+            //shiftsOverGp = float.Parse(cols[19]),
+            //fowPercent = float.Parse(cols[20])
+
+            var field = split[0];
+            var op = split[1];
+            var val = split[2];
+            switch (field.ToLower()) { //convert certain values and throw an error if none match
+                case "player":
+                case "team":
+                case "pos":
+                case "gp":
+                case "g":
+                case "a":
+                case "p":
+                case "pim":
+                case "ppg":
+                case "ppp":
+                case "shg":
+                case "shp":
+                case "gwg":
+                case "otg":
+                case "s":
+                    break;
+                case "+/-":
+                    field = "plusMinus";
+                    break;
+                case "p/gp":
+                    field = "pOverGp";
+                    break;
+                case "s%":
+                    field = "sPercent";
+                    break;
+                case "toi/gp":
+                    field = "toiOverGp";
+                    break;
+                case "shifts/gp":
+                    field = "shiftsOverGp";
+                    break;
+                case "fow%":
+                    field = "fowPercent";
+                    break;
+                default:
+                    throw new Exception($"{val} is not a valid column name");
+            }
+
+            var where = $"{field} {op} {val}";
             var res = list.Where(where);
             return res.ToList();
+        }
+
+        static void printResult(List<csvRow> res) {
+            foreach (var r in res)
+            {
+                Console.WriteLine(r.name);
+            }
+
         }
         static void Main(string[] args)
         {
             string query = "";
             do {
-                Console.WriteLine("Enter a query ie \"GP >= 50\". Type exit to exit");
+                Console.WriteLine("Enter a query ie \"GP >= 50\". Type e to exit");
                 query = Console.ReadLine();
                 
-                if (query == "exit") { break; }
+                if (query == "e") { break; }
                 var rows = csvParser.getRows();
-                try { 
-                    var res = handleQuery(query);
+                try {
+                    var res = rows;
+                    if (query != "") { res = handleQuery(query); }
+                    printResult(res);
                     Console.WriteLine($"original count: {rows.Count}, filtered count: {res.Count}");
                 } catch {
                     Console.WriteLine("Invalid query");
                 }
             } while (true);
-
         }
 
         public static class csvParser {
@@ -81,7 +158,7 @@ namespace nhlconsole {
         }
 
 
-        public class csvRow {
+        public class csvRow { //just used to store data
             public string name;
             public string team;
             public string pos; //could change to enum 
@@ -104,6 +181,5 @@ namespace nhlconsole {
             public float shiftsOverGp;
             public float fowPercent;
         }
-
     }
 }
